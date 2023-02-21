611c6ef3a31f40ab84c0dd364f8b03f50526a6d8
金绘 <11615607+gold-painting@user.noreply.gitee.com>
2023-02-21 20:08:34 +0800

    cnode

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Header/>
+    <div class="main">
+      <!-- name 指的是路由main:PostList -->
+      <router-view name="slidebar"></router-view>
+      <router-view name="main"></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import PostList from './components/PostList.vue'
+export default {
+  name: 'App',
+  components: {
+    Header,
+    PostList
+  }
+}
+</script>
+
+<style>
+.main {
+  width: 80%;
+  margin: 0 auto;
+}
+</style>
